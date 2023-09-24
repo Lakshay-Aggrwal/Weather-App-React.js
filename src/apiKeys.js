029391a05d92c9297fd8200c501829a998563c09
Lakshay Aggarwal <89836321+Lakshay-Aggrwal@users.noreply.github.com>
2023-09-24 14:19:18 +0530

    apiKeys.js

--- src/apiKeys.js
+++ src/apiKeys.js
@@ -1,5 +1,5 @@
 // Visit https://api.openweathermap.org & then signup to get our API keys for free
 module.exports = {
-  key: "{Your API Key Here}",
+  key: "9c349988c974f389f221dfb80d820187",
   base: "https://api.openweathermap.org/data/2.5/",
 };
